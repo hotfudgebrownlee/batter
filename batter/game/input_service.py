d0e6ebdc7b8e30420a60c32022763e2b6c589d5f
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-25 16:55:01 -0700

    control actions, input service

--- batter/game/input_service.py
+++ batter/game/input_service.py
@@ -5,27 +5,22 @@ from asciimatics.event import KeyboardEvent
 class InputService:
     """Detects player input. The responsibility of the class of objects
      is to detect and communicate player keypresses.
-
     Stereotype: 
         Service Provider
-
     Attributes:
         _screen (Screen): An Asciimatics screen.
-        _keys (list): Points for up, dn, lt, rt.
+        _keys (list): Points for lt, rt.
     """
 
     def __init__(self, screen):
         """The class constructor."""
         self._screen = screen
         self._keys = {}
-        self._keys[119] = Point(0, -1) # w
-        self._keys[115] = Point(0, 1) # s
         self._keys[97] = Point(-1, 0) # a
         self._keys[100] = Point(1, 0) # d
-        
+
     def get_direction(self):
         """Gets the selected direction for the given player.
-
         Returns:
             Point: The selected direction.
         """
