48bc4b762403295c5ce4a880f51f97182608ffff
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-02-24 19:05:49 -0700

    handle collisions

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -16,3 +16,20 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
+        paddle = cast["paddle"][0] # there's only one
+        ball = cast["ball"][0] # there's only one
+        bricks = cast["brick"]
+        x = ball.get_velocity().get_x()
+        y = ball.get_velocity().get_y()
+        for brick in bricks:
+            if ball.get_position().equals(brick.get_position()):
+                del brick
+                ball.set_velocity((x * -1),(y * -1))
+        if ball.get_position().equals(paddle.get_position()):
+            ball.set_velocity(x,(y * -1))
+        elif ball.get_position().get_x() == (0 or constants.MAX_X):
+            ball.set_velocity((x * -1),y)
+        elif ball.get_position().get_y() == 0:
+            ball.set_velocity(x,(y * -1))
+        elif ball.get_position().get_y() == constants.MAX_Y:
+            del ball
